63940f83fb4f492b37eeb7db24bf75957f6877a7
Dr Laurence E. Day <laurence.e.day@outlook.com>
2021-09-30 10:13:11 +0100

    Added IR and started Sharpe Ratio
    Need to finish Sortino and stdev of returns
    
    Can probably refactor a lot of this further

--- analytics.ts
+++ analytics.ts
@@ -17,7 +17,7 @@ function get_pct_change(last_n: number, data: Array<number>) {
         return data[data.length - 1] / data[0] - 1;
     }
     else {
-        return data[data.length - 1] / data[data.length - (1 + last_n)] - 1;
+        return data[data.length - 1] / data[data.length - last_n] - 1;
     }
 }
 
@@ -42,15 +42,82 @@ function r2( etf_queried   : Array<number>
     return ss.sampleCorrelation( etf_queried, etf_benchmark ) ** 2;
 }
 
+function get_timeslice( last_n : number
+                      , data : Array<number>
+                      )
+{
+    if (last_n == 0) {
+        return data;
+    } else
+    {
+        return data.slice(data.length - last_n, data.length);
+    }
+}
+
+function get_tracking_error( etf_queried : Array<number>
+                           , etf_benchmark : Array<number>
+                           )
+{
+    let etf_diff: Array<number> = [];
+    
+    // Can iterate over length of one: assumption that they're same length
+    for (let i = 0; i < etf_queried.length; i++) {
+        etf_diff[i] = (etf_queried[i] - etf_benchmark[i]);
+    }
+
+    return ss.standardDeviation(etf_diff);
+}
+
+function information_ratio( lookback_period : number
+                          , etf_queried     : Array<number>
+                          , etf_benchmark   : Array<number>
+                          )
+{
+    
+    // Filter down the dataset to appropriate period
+    etf_queried = get_timeslice(lookback_period, etf_queried);
+    etf_benchmark = get_timeslice(lookback_period, etf_benchmark);
+
+    let tracking_error = get_tracking_error(etf_queried, etf_benchmark);
+    
+    let port_returns = get_pct_change(0, etf_queried);
+    let bench_returns = get_pct_change(0, etf_benchmark);
+    
+    return (port_returns - bench_returns) / tracking_error;
+  
+}
+
+function sharpe_ratio( rfr : number
+                     , lookback_period : number,
+                     , etf_queried : Array<number>
+                     , etf_benchmark : Array<number>
+                     )
+{
+    // Filter down the dataset to appropriate period
+    etf_queried = get_timeslice(lookback_period, etf_queried);
+    etf_benchmark = get_timeslice(lookback_period, etf_benchmark);
+    
+    let stdev_returns = 0 // TODO: stdev of portfolio returns, not just P - B
+    
+    // need to finish this
+    return 0;
+}
+
+// TODO: Sortino Ratio
+// TODO: Standard Deviation of Single Array
+
 const prices1 = [1.4, 1.69, 1.52, 1.58, 1.59];
 const prices2 = [1.3, 1.54, 1.29, 1.98, 1.67];
+const test_rfr = 0.042
+const lookback = 4
 
 function test_stats() {
     
-    console.log(`Comparing %s to %s...`, prices1, prices2);
+    console.log(`Comparing %s to %s...\n`, prices1, prices2);
     console.log(`Beta: %d`, beta(prices1, prices2));
-    console.log(`Alpha: %d`, alpha(0.0403, 0, prices1, prices2));
+    console.log(`Alpha: %d`, alpha(test_rfr, lookback, prices1, prices2));
     console.log(`R2: %d`, r2(prices1, prices2));
+    console.log(`IR: %d`, information_ratio(lookback, prices1, prices2));
 
 }
 
